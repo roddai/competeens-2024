4774bab853d17bc094c3ce4aec8894b87f2408ec
Rodrigo <roddai@hotmail.com>
2024-02-06 14:53:36 -0300

    ajusta link

--- script.js
+++ script.js
@@ -85,10 +85,10 @@ const getArrayWithScores = ({ teens }) => teens
   .sort((a, b) => b - a);
 
 const clickBackButton = () => backButton
-  .addEventListener('click', () => window.location = 'http://127.0.0.1:5500/index.html?');
+  .addEventListener('click', () => window.location = 'https://roddai.github.io/competeens-2024');
 
 window.onload = () => {
-  if (window.location.href === 'http://127.0.0.1:5500/index.html' || window.location.href === 'http://127.0.0.1:5500/index.html?') {
+  if (window.location.href === 'https://roddai.github.io/competeens-2024' || window.location.href === 'https://roddai.github.io/competeens-2024?') {
     addOptions(data);
     clickEntryButton(data);
   } else {
