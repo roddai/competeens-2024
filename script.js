fa6d010af39a617686da2964e29677c86b728a98
Rodrigo <roddai@hotmail.com>
2024-02-06 17:09:15 -0300

    ajusta links

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ const verifyLogin = (arrayTeens, teenName, password) => {
   const objectTeen = arrayTeens.find(({ name }) => name === teenName);
 
   if (objectTeen.password === password) {
-    // window.location.href = 'http://127.0.0.1:5500/teen-score.html';
-    window.location.href = 'https://roddai.github.io/competeens-2024/teen-score.html';
+    window.location.href = 'http://127.0.0.1:5500/teen-score.html';
+    // window.location.href = 'https://roddai.github.io/competeens-2024/teen-score.html';
     testFunction(objectTeen);
   } else {
     throw new Error('Usuário ou senha inválido(a)!');
@@ -86,10 +86,11 @@ const getArrayWithScores = ({ teens }) => teens
   .sort((a, b) => b - a);
 
 const clickBackButton = () => backButton
-  .addEventListener('click', () => window.location = 'https://roddai.github.io/competeens-2024');
+  // .addEventListener('click', () => window.location = 'https://roddai.github.io/competeens-2024');
+  .addEventListener('click', () => window.location = 'http://127.0.0.1:5500/index.html');
 
 window.onload = () => {
-  if (window.location.href === 'https://roddai.github.io/competeens-2024' || window.location.href === 'https://roddai.github.io/competeens-2024?' || window.location.href === 'http://127.0.0.1:5500/index.html' || window.location.href === 'http://127.0.0.1:5500/index.html?') {
+  if (window.location.href === 'http://127.0.0.1:5500/index.html' || window.location.href === 'http://127.0.0.1:5500/index.html?') {
     addOptions(data);
     clickEntryButton(data);
   } else {
